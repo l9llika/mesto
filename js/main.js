6cecf9ee196414cbe5a2ae3093bb6fb886534397
L9LLIKA <L9LLIKA@yandex.ru>
2022-06-27 18:55:00 +0700

    Create all section for desktop
    All sections and css. desktop version is already. add script for like button

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let placeChoice = document.querySelectorAll('.place__choiсe');
+
+[].forEach.call(placeChoice, function (placeChoice) {
+  placeChoice.addEventListener('click', function () {
+    placeChoice.classList.toggle('place__choice_active')
+  });
+});
