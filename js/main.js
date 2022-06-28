cad3302ea4acdd34d9318fd0ab05fe1e92972f0b
L9LLIKA <L9LLIKA@yandex.ru>
2022-06-28 07:25:20 +0700

    create adaptive for all sections

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ let placeChoice = document.querySelectorAll('.place__choiсe');
 
 [].forEach.call(placeChoice, function (placeChoice) {
   placeChoice.addEventListener('click', function () {
-    placeChoice.classList.toggle('place__choice_active')
+    placeChoice.classList.toggle('place__choice_active');
   });
 });
