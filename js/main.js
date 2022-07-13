d79645f6083328b68c81ebdd7f0df8ee02af9db7
L9LLIKA <L9LLIKA@yandex.ru>
2022-07-14 00:47:47 +0700

    create popup for add places, create initial places

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-/** funtion for like button
-const placeChoice = document.querySelectorAll('.place__choiсe');
+//  funtion for like button
+const placeChoice = document.querySelectorAll('.place__choice');
 
 [].forEach.call(placeChoice, function (placeChoice) {
   placeChoice.addEventListener('click', function () {
     placeChoice.classList.toggle('place__choice_active');
   });
 });
-*/
+
 
 //** function for close and open popupform */
 const popupElement = document.querySelector('.popup');
@@ -40,6 +40,85 @@ function submitPopupForm(evt) {
 }
 
 formElement.addEventListener('submit', submitPopupForm);
+
 //** open and close popup listeners */
 profileEditBtn.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
+
+// initial places from vendor
+const initialPlaces = [{
+    name: 'Монблан',
+    link: './images/monblanc.jpg'
+  },
+  {
+    name: 'Аликанте',
+    link: './images/alicante.jpg'
+  },
+  {
+    name: 'Ай-Петри',
+    link: './images/aipetri.jpg'
+  },
+  {
+    name: 'Берхофен замок в Швейцарии',
+    link: './images/berhofen.jpg'
+  },
+  {
+    name: 'Анталья',
+    link: './images/goynuk.jpg'
+  },
+  {
+    name: 'Алтай Катунь',
+    link: './images/altay.jpg'
+  },
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  }, {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  }, {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  }, {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  }, {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  }, {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+// reneder initial places in html
+const placeElements = document.querySelector('.elements')
+
+initialPlaces.forEach(function (place) {
+  const placeTemplate = document.querySelector('.place-template').content.querySelector('.place').cloneNode(true);
+  placeTemplate.querySelector('.place__name').textContent = place.name;
+  placeTemplate.querySelector('.place__image').src = place.link;
+  placeTemplate.querySelector('.place__image').alt = place.name;
+  placeTemplate.querySelector('.place__choice-btn').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('place__choice-btn_active');
+  });
+  placeTemplate.querySelector('.place__trash-btn').addEventListener('click', function (evt) {
+    placeTemplate.remove();
+  });
+  placeElements.appendChild(placeTemplate);
+});
+
+// open create new places popup
+const popupAddPlace = document.querySelector('.popup__add-place');
+const popupAddPlaceCloseBtn = popupAddPlace.querySelector('.popup__close-btn');
+const placeAddBtn = profileElement.querySelector('.profile__add-btn');
+console.log(popupAddPlaceCloseBtn);
+
+let openAddPopup = function () {
+  popupAddPlace.classList.add('popup_opened');
+}
+let closeAddPopup = function () {
+  popupAddPlace.classList.remove('popup_opened');
+}
+
+placeAddBtn.addEventListener('click', openAddPopup);
+popupAddPlaceCloseBtn.addEventListener('click', closeAddPopup);
